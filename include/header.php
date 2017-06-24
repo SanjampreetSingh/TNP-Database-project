c4cb75050aaa016ac80fc1f88b88ea991db00f70
Sanjampreet Singh <sanjam531@gmail.com>
2017-06-24 15:00:51 +0530

    Made Nav bar and header and included css links

---
+++ include/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="keywords" content="GNDEC,GNE" />
+	<meta name="description" content="" />
+	<meta name="Author" content="Sanjampreet Singh" />
+	<title>Material Design Bootstrap</title>
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"  type="text/css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css"  type="text/css">
+	<!-- MD Bootstrap -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.3.0/css/mdb.min.css" type="text/css">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-toggleable-md navbar-dark bg-primary">
+			<div class="container">
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#collapseEx12" aria-controls="collapseEx2" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<a class="navbar-brand" href="#">
+				<div class="hidden-md-down">Academic Section</div>
+				</a>
+				<div class="collapse navbar-collapse" id="collapseEx12">
+					<ul class="nav navbar-nav nav-flex-icons ml-auto">
+					<a href="#" id="show_login" class="nav-link waves-effect waves-light"><i class="fa fa-sign-in"></i><span class="hidden-sm-down">Log In</span></a>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</header>
+</body>
