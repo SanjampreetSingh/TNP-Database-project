88cfcef818f24ba307414a92182de665a92dfced
Sanjampreet Singh <sanjam531@gmail.com>
2017-11-08 13:51:20 +0530

    Added more to ui and profile

--- admin/student/pages/profile.php
+++ admin/student/pages/profile.php
@@ -8,40 +8,275 @@
       <div class="row">
         <div class="container">
           <form action="" method="post" enctype="multipart/form-data">
-            
-            <legend class=""> Personal Details
-                <div class="row">
-                  <div class="col-xs-4">
-                    <div class="form-group">
-                      <label for="email" class="control-label"> 
-                        First Name 
-                      </label>
-                      <input type="text" disabled="disabled" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
-                    </div>
-                  </div>
-                  <div class="col-xs-4">
-                    <div class="form-group">
-                      <label for="email" class="control-label"> 
-                        Middle Name 
-                      </label>
-                      <input type="text" disabled="disabled" name="middle_name" id="middle_name" class="form-control input-sm" placeholder="Middle Name">
-                    </div>
-                  </div>
-                  <div class="col-xs-4">
-                    <div class="form-group">
-                      <label for="email" class="control-label"> 
-                        Last Name 
-                      </label>
-                      <input type="text" disabled="disabled" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
-                    </div>
+
+            <!-- Personal Details -->
+            <legend> <span class="text-light-blue">Personal Details</span>
+              <div class="row">
+                <div class="col-xs-4">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      First Name 
+                    </label>
+                    <input type="text" disabled="disabled" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
                   </div>
                 </div>
-            </legend>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- /.content -->
+                <div class="col-xs-4">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Middle Name 
+                    </label>
+                    <input type="text" disabled="disabled" name="middle_name" id="middle_name" class="form-control input-sm" placeholder="Middle Name">
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Last Name 
+                    </label>
+                    <input type="text" disabled="disabled" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
+                  </div>
+                </div>
+              </div>
+              <!-- //////////////////////////////////// -->
+              <div class="row">
+                <div class="col-xs-4">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Date Of Birth 
+                    </label>
+                    <input type="text" disabled="disabled" name="DOB" id="DOB" class="form-control input-sm" placeholder="Date of Birth">
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Contact Number 
+                    </label>
+                    <input type="text" disabled="disabled" name="contact_number" id="contact_number" class="form-control input-sm" placeholder="Contact Number ">
+                  </div>
+                </div>
+                <div class="col-xs-4">
+                  <div class="form-group">
+                    <label for="email" class="control-label"> 
+                      Email 
+                    </label>
+                    <input type="text" disabled="disabled" name="email" id="email" class="form-control input-sm" placeholder="Email">
+                  </div>
+                </div>
+              </div>
+              <!-- //////////////////////////////////// -->
+              <div class="row">
+                <div class="col-xs-2">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Age 
+                    </label>
+                    <input type="text" disabled="disabled" name="age" id="age" class="form-control input-sm" placeholder="Age">
+                  </div>
+                </div>
+                <div class="col-xs-2">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Gender 
+                    </label>
+                    <input type="text" disabled="disabled" name="gender" id="gender" class="form-control input-sm" placeholder="Gender">
+                  </div>
+                </div>
+                <div class="col-xs-2">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Category 
+                    </label>
+                    <input type="text" disabled="disabled" name="category" id="category" class="form-control input-sm" placeholder="Category">
+                  </div>
+                </div>
+                <div class="col-xs-2">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Blood Group 
+                    </label>
+                    <input type="text" disabled="disabled" name="blood_group" id="blood_group" class="form-control input-sm" placeholder="Blood Group">
+                  </div>
+                </div>
+                <div class="col-xs-2">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Height 
+                    </label>
+                    <input type="text" disabled="disabled" name="height" id="height" class="form-control input-sm" placeholder="Height">
+                  </div>
+                </div>
+                <div class="col-xs-2">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Weight 
+                    </label>
+                    <input type="text" disabled="disabled" name="weight" id="weight" class="form-control input-sm" placeholder="Weight">
+                  </div>
+                </div>
+              </div>
+              <!-- //////////////////////////////////// -->
+              <div class="row">
+                <div class="col-xs-6">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                      Father's Name 
+                    </label>
+                    <input type="text" disabled="disabled" name="fName" id="fName" class="form-control input-sm" placeholder="Father's Name">
+                  </div>
+                </div>
+                <div class="col-xs-6">
+                  <div class="form-group">
+                    <label class="control-label"> 
+                     Father's Contact Number 
+                   </label>
+                   <input type="text" disabled="disabled" name="father_contact_number" id="father_contact_number" class="form-control input-sm" placeholder="Father's Contact Number ">
+                 </div>
+               </div>
+             </div>
+             <!-- //////////////////////////////////// -->
+             <div class="row">
+              <div class="col-xs-6">
+                <div class="form-group">
+                  <label class="control-label"> 
+                    Mother's Name 
+                  </label>
+                  <input type="text" disabled="disabled" name="mName" id="mName" class="form-control input-sm" placeholder="Mother's Name">
+                </div>
+              </div>
+              <div class="col-xs-6">
+                <div class="form-group">
+                  <label class="control-label"> 
+                   Mother's Contact Number 
+                 </label>
+                 <input type="text" disabled="disabled" name="mother_contact_number" id="mother_contact_number" class="form-control input-sm" placeholder="Mother's Contact Number ">
+               </div>
+             </div>
+           </div>
+           <!-- //////////////////////////////////// -->
+         </legend>
+
+
+           <!-- Address Details -->
+           <legend> <span class="text-light-blue">Address Details</span>
+             <div class="row">
+              <div class="col-xs-12">
+                <div class="form-group">
+                  <label class="control-label"> 
+                    Address
+                  </label>
+                  <input type="text" disabled="disabled" name="Address" id="address" class="form-control input-sm" placeholder="University Roll Number">
+                </div>
+              </div>
+              <div class="col-xs-6">
+                <div class="form-group">
+                  <label class="control-label"> 
+                    Class Roll Number
+                  </label>
+                  <input type="text" disabled="disabled" name="class_roll_number" id="class_roll_number" class="form-control input-sm" placeholder="Class Roll Number">
+                </div>
+              </div>
+            </div>
+            <!-- ////////////////////////////////// -->
+            <div class="row">
+             <div class="col-xs-3">
+               <div class="form-group">
+                 <label class="control-label"> 
+                   Batch
+                 </label>
+                 <input type="text" disabled="disabled" name="batch" id="batch" class="form-control input-sm" placeholder="Batch">
+               </div>
+             </div>
+             <div class="col-xs-3">
+               <div class="form-group">
+                 <label class="control-label"> 
+                   Shift
+                 </label>
+                 <input type="text" disabled="disabled" name="shift" id="shift" class="form-control input-sm" placeholder="Shift">
+               </div>
+             </div>
+             <div class="col-xs-3">
+               <div class="form-group">
+                 <label class="control-label"> 
+                   Section
+                 </label>
+                 <input type="text" disabled="disabled" name="section" id="section" class="form-control input-sm" placeholder="Section">
+               </div>
+             </div>
+             <div class="col-xs-3">
+               <div class="form-group">
+                 <label class="control-label"> 
+                   Stream
+                 </label>
+                 <input type="text" disabled="disabled" name="stream" id="stream" class="form-control input-sm" placeholder="Stream">
+               </div>
+             </div>
+           </div>
+         </legend>
+
+
+
+         <!-- Academic Details -->
+         <legend> <span class="text-light-blue">Academic Details</span>
+           <div class="row">
+            <div class="col-xs-6">
+              <div class="form-group">
+                <label class="control-label"> 
+                  University Roll Number
+                </label>
+                <input type="text" disabled="disabled" name="univ_roll_number" id="univ_roll_number" class="form-control input-sm" placeholder="University Roll Number">
+              </div>
+            </div>
+            <div class="col-xs-6">
+              <div class="form-group">
+                <label class="control-label"> 
+                  Class Roll Number
+                </label>
+                <input type="text" disabled="disabled" name="class_roll_number" id="class_roll_number" class="form-control input-sm" placeholder="Class Roll Number">
+              </div>
+            </div>
+          </div>
+          <!-- ////////////////////////////////// -->
+          <div class="row">
+           <div class="col-xs-3">
+             <div class="form-group">
+               <label class="control-label"> 
+                 Batch
+               </label>
+               <input type="text" disabled="disabled" name="batch" id="batch" class="form-control input-sm" placeholder="Batch">
+             </div>
+           </div>
+           <div class="col-xs-3">
+             <div class="form-group">
+               <label class="control-label"> 
+                 Shift
+               </label>
+               <input type="text" disabled="disabled" name="shift" id="shift" class="form-control input-sm" placeholder="Shift">
+             </div>
+           </div>
+           <div class="col-xs-3">
+             <div class="form-group">
+               <label class="control-label"> 
+                 Section
+               </label>
+               <input type="text" disabled="disabled" name="section" id="section" class="form-control input-sm" placeholder="Section">
+             </div>
+           </div>
+           <div class="col-xs-3">
+             <div class="form-group">
+               <label class="control-label"> 
+                 Stream
+               </label>
+               <input type="text" disabled="disabled" name="stream" id="stream" class="form-control input-sm" placeholder="Stream">
+             </div>
+           </div>
+         </div>
+       </legend>
+     </form>
+   </div>
+ </div>
+</div>
+</section>
+<!-- /.content -->
 </div>
 <!-- /.content-wrapper
